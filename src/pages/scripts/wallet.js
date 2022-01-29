ad34bdef474188d54703d5ed5ce417a1c86c493a
Johnexzy <obajohn75@gmail.com>
2022-01-29 07:49:24 +0100

    added conf

--- src/pages/scripts/wallet.js
+++ src/pages/scripts/wallet.js
@@ -1,23 +1,3 @@
-<template>
-  <q-page v-if="!getAccounts" class="flex flex-center">
-    <q-btn @click="connectMetaMask" color="primary" class="q-ma-md"
-      >Connect with MetaMask</q-btn
-    >
-    <q-btn @click="WalletConnect" color="secondary" class="q-ma-md"
-      >Connect with WalletConnect
-    </q-btn>
-  </q-page>
-  <q-page v-else class="flex flex-center">
-    <div>{{ getAccounts }}</div>
-    <q-btn
-      color="white"
-      text-color="black"
-      @click="switchAccount"
-      label="Switch Account"
-    />
-  </q-page>
-</template>
-<script setup>
 import { computed, onMounted } from "vue";
 import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
@@ -89,38 +69,85 @@ async function WalletConnect() {
 
   // walletConnectProvider.qrcodeModal.open();
 }
-
-/**
- * Restore existing Wallet Connection (Metamask)
- */
 const checkIfWalletIsConnected = async () => {
-
+  /*
+   * First: check if metamask is connected
+   * Second: check if wallet connect is connected and active
+   */
+  console.log("john");
   console.log("looking for connections");
-  await isMetamaskConnected()
+  if (await isMetamaskConnected()) {
+    // metaMaskProvider.on("disconnect", console.log("disconnected"));
+
+    const metaMaskProvider = await detectEthereumProvider({
+      mustBeMetaMask: true,
+    });
+    metaMaskProvider.on("disconnect", function () {
+      console.log("disconnected");
+    });
+    const accounts = await metaMaskProvider.request({ method: "eth_accounts" });
+    console.log(accounts);
+    if (accounts.length !== 0) {
+      web3 = new Web3(metaMaskProvider);
+      $store.commit("setUserAccount", accounts[0]);
+      return;
+    }
+  } else if (isWCconected()) {
+    console.log("Wallet Connect is connected");
+    walletConnectProvider = new WalletConnectProvider({
+      infuraId: "e8f8c34dc1dd47b2a00d6569d52b8ec7", // Required
+      qrcodeModalOptions: {
+        mobileLinks: ["metamask", "trust"],
+      },
+    });
+
+    // await walletConnectProvider.enable();
+    web3 = new Web3(walletConnectProvider);
+
+    //  Get Accounts
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length !== 0) {
+      // const signedMessage = await web3.eth.sign(
+      //   "Welcome to Afroapes: The Origin",
+      //   accounts[0]
+      // );
+      $store.commit("setUserAccount", accounts[0]);
+      // console.log(accounts)
+      console.log(web3.eth);
+      // setUserAccount.value = accounts;
+      //  Get Chain Id
+      const chainId = await web3.eth.chainId();
+      console.log(chainId);
+
+      //  Get Network Id
+      const networkId = await web3.eth.net.getId();
+      console.log(networkId);
+    }
+  }
 };
-/**
- * Look for existing metamask connection
- */
+
 const isMetamaskConnected = async () => {
   const metaMaskProvider = await detectEthereumProvider({
     mustBeMetaMask: true,
   });
-
-  metaMaskProvider.on("disconnect", function () {
-    alert("disconnected");
-  });
-  const accounts = await metaMaskProvider.request({ method: "eth_accounts" });
-  console.log(accounts);
-  if (accounts.length !== 0) {
-    web3 = new Web3(metaMaskProvider);
-    $store.commit("setUserAccount", accounts[0]);
-    return;
+  if (metaMaskProvider && metaMaskProvider.isConnected()) {
+    // console.log("Make sure you have metamask!");
+    return true;
   }
-
   return false;
 };
-
-
+const isWCconected = () => {
+  walletConnectProvider = new WalletConnectProvider({
+    infuraId: "e8f8c34dc1dd47b2a00d6569d52b8ec7", // Required
+    qrcodeModalOptions: {
+      mobileLinks: ["metamask", "trust"],
+    },
+  });
+  if (walletConnectProvider && walletConnectProvider.connected) {
+    return true;
+  }
+  return false;
+};
 const switchAccount = async () => {
   if (await isMetamaskConnected()) {
     const metaMaskProvider = await detectEthereumProvider({
@@ -135,10 +162,16 @@ const switchAccount = async () => {
       ],
     });
     window.location.reload();
+    // const accounts = await metaMaskProvider.request({ method: "eth_accounts" });
+    // console.log(accounts)
+    // if (accounts.length !== 0) {
+    //   web3 = new Web3(metaMaskProvider);
+    //   $store.commit("setUserAccount", accounts[0]);
+    //   return;
+    // }
   }
 };
 
 onMounted(() => {
   checkIfWalletIsConnected();
 });
-</script>
